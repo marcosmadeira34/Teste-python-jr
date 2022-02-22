d975ea8c1e460e8cdd928cc66bfe37f3c33a9301
Marcos Madeira <mmadeirasilva5@gmail.com>
2022-02-22 17:23:56 -0300

    Initial addition of files

---
+++ vough_backend/api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from . import views
+from api.routes import CustomRouter
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+
+
+
+routers = CustomRouter()
+routers.register('orgs', views.OrganizationViewSet)
+
+
+# rota de documentação swagger-ui (user interface) 
+urlpatterns =[
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+
+
+    path('', include(routers.urls))
+]
+
+
+
+
