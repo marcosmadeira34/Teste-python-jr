267bc4fb0ce520cc057c1690dcada50c8e513ec0
Marcos Madeira <mmadeirasilva5@gmail.com>
2022-02-23 15:13:59 -0300

    Adicionando arquivo de Setup

---
+++ vough_backend/api/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'Teste-Python-Jr',
+    version = '1.0.0',
+    description = 'Consulta organizações na REST API GITHUB e classificação por nível de prioridade\
+        via cálculo do número de membros na organização + número de repositórios públicos'
+
+)
